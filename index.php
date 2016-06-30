033a746a9c0f85126107248585683a95cc93b8c4
xiplus <huangxuanyuxiplus@gmail.com>
2016-06-30 20:50:20 +0800

    End of Support page

--- index.php
+++ index.php
@@ -3,41 +3,22 @@
 $title="";
 if(isset($_GET["class"])){
 	$url="http://w3.tnfsh.tn.edu.tw/deanofstudies/course/c101".urlencode($_GET["class"]).".html";
-	$html=@file_get_contents($url);
-	$html=iconv("BIG5", "UTF-8//IGNORE", $html);
-	$html=preg_replace('/href=".*?t(.*?).html"/','href="?teach=$1"',$html);
-	$html=preg_replace('/href=".*?_(.*?).html"/','href="?main=$1"',$html);
-	$title=urlencode($_GET["class"]);
 }
 else if(isset($_GET["teach"])){
 	$url="http://w3.tnfsh.tn.edu.tw/deanofstudies/course/t".urlencode($_GET["teach"]).".html";
-	$html=@file_get_contents($url);
-	$html=iconv("BIG5", "UTF-8//IGNORE", $html);
-	$html=preg_replace('/href=".*?c101(.*?).html"/','href="?class=$1"',$html);
-	$html=preg_replace('/href=".*?_(.*?).html"/','href="?main=$1"',$html);
-	preg_match("/blue.*?>(.+?)<\/.+?> *?教師課表/", $html, $match);
-	$title=$match[1];
 }
 else if(isset($_GET["main"])){
 	$url="http://w3.tnfsh.tn.edu.tw/deanofstudies/course/_".urlencode($_GET["main"]).".html";
-	$html=@file_get_contents($url);
-	$html=iconv("BIG5", "UTF-8//IGNORE", $html);
-	$html=preg_replace('/href=".*?c101(.*?).html"/','href="?class=$1"',$html);
-	$html=preg_replace('/href=".*?t(.*?).html"/','href="?teach=$1"',$html);
-	if ($_GET["main"]=="classindex") $title="班級索引";
-	else if ($_GET["main"]=="teachindex") $title="教師索引";
 }
 else {
 	$url="http://w3.tnfsh.tn.edu.tw/deanofstudies/course/course.html";
-	$html=@file_get_contents($url);
-	$html=iconv("BIG5", "UTF-8//IGNORE", $html);
-	$html=preg_replace('/href=".*?_(.*?).html"/','href="?main=$1"',$html);
 }
 ?>
 <head>
 	<meta charset="UTF-8">
-	<title>南一中<?php echo $title; ?>課表</title>
-	<meta name=viewport content="width=600, initial-scale=1">
+	<title>南一中課表</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="refresh" content="15; url=<?php echo $url; ?>">
 	<link rel="Shortcut Icon" href="res/icon/60.png">
 	<link rel="apple-touch-icon" href="res/icon/60.png">
 	<link rel="apple-touch-icon" sizes="76x76" href="res/icon/76.png">
@@ -47,11 +28,8 @@ else {
 <body bgColor="#D0D0D0">
 <center>
 <h2>臺南一中課表查詢系統 for UTF-8</h2>
-若你想要使用 <b>BIG5</b> 或是想看<b>亂碼</b>，建議你使用<a href="http://w3.tnfsh.tn.edu.tw/deanofstudies/course/course.html">正版網站</a><br><br>
-<?php
-$html=str_replace("big5", "UTF-8", $html);
-echo $html;
-?>
+由於臺南一中課表已修正編碼設定，能夠於手機瀏覽器正常顯示，所以此專案已不再必要，決定此專案將不再更新，真是可喜可賀。<br>
+即將為你重新導向至正版網站，或是<a href="<?php echo $url; ?>">點我立即前往</a><br><br>
 <hr>
 <?php
 @include("function/Xiplus-Facebook-Badge/badge.php");
